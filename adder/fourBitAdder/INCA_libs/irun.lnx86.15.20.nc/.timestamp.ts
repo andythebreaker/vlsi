f8d98887349bdc73623783108800c48c23c4f008
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-10-07 21:58:13 +0800

    SHup20191007 Version = 12

---
+++ adder/fourBitAdder/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
@@ -0,0 +1,1 @@
+1570455839 /home/ncku_class/vlsi2019/vlsi2006/mydir/vlsi/adder/fourBitAdder/fourBitAdder.v
---
+++ adder/fourBitAdder/fourBitAdder.v
@@ -0,0 +1,37 @@
+`timescale 1 ns /1 ps
+`include "fulladder.v"
+module fourBitAdder(
+	c_in,a,b,//in
+	sum,c_out//out
+);
+//=============================================
+//NOTAM:
+//do not write:
+//
+//module fourBitAdder(
+//	input c_in,a,b,
+//	output sum,c_out
+//);
+//
+//input [3:0] a,b;
+//...~
+//
+//==> repeat announcement :: u can't do "input" on smae varib. two times
+//<!will cause error>
+//
+//(2019.10.7)
+//
+//============================================= 
+input [3:0] a,b;
+input c_in;
+output [3:0] sum;
+output c_out;
+fulladder f0 (
+.a(),
+.b(),
+.c(),
+.sum(),
+.c_out()
+);
+
+endmodule
