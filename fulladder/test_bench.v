2c454ed15deab4bd6291b3abefa594750826059c
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-10-07 20:56:57 +0800

    SHup20191007 Version = 8

--- fulladder/test_bench.v
+++ fulladder/test_bench.v
@@ -1,3 +1,5 @@
+`include "fulladder"
+`timescale 1 ns / 1 ps
 module test_bench;
 reg a;
 reg b;
@@ -12,7 +14,24 @@ $monitor($time," || a= %b | b= %b | c= %b | s= %b | os= %b",a,b,c,s,co);
 a=1'b0;
 b=1'b0;
 c=1'b0;
+#5 a=
 end
+//====================chart========================
+//for all abc -> output S/C
+//000->0/0
+//001->1/0
+//011->0/1
+//111->1/1
+//a|b|c/S-c
+//000/0-0
+//001/1-0
+//010/1-0
+//100/1-0
+//011/0-1
+//110/0-1
+//101/0-1
+//111/1-1
+//============all we need to test==================
 //always #50 begin
 //if
 endmodule
