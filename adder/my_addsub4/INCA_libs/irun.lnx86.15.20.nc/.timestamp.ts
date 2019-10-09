d352b73657cbb89e9b98380613d0db6eb13e8bc5
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-10-09 15:15:07 +0800

    SHup20191009 Version = 19

--- adder/my_addsub4/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
+++ adder/my_addsub4/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
@@ -1,1 +1,2 @@
+1570602945 /home/ncku_class/vlsi2019/vlsi2006/mydir/vlsi/adder/my_addsub4/main.v
 1570601556 /home/ncku_class/vlsi2019/vlsi2006/mydir/vlsi/adder/addsub4/main.v
--- adder/my_addsub4/addSub4.v
+++ adder/my_addsub4/addSub4.v
@@ -1,9 +1,13 @@
-module addSub4(input op, input signed [3:0] a, input signed [3:0] b, 
+module addSub4(input op, input signed [3:0] a, input signed [3:0] b,input c_in, 
                output signed [3:0] sum, output c_out);
 
-wire [3:0] bop;
+wire [3:0] contrast;
+wire [3:0] sum_withoutP;
+wire [3:0] Cout_withoutP;
 
-xor4 x1(b, {op,op,op,op}, bop);
-adder4 a1(a, bop, op, sum, c_out);
+xor4 x1(b, {op,op,op,op}, contrast);
+adder4 a1(a, contrast, c_in, sum_withoutP, Cout_withoutP);
+
+adder4 a2(sum_withoutP,
 
 endmodule
