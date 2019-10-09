cb5ea371e459abce8096b728b2498ec060ae9d8c
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-10-09 15:36:01 +0800

    SHup20191009 Version = 21

--- adder/my_addsub4/half_adder4.v
+++ adder/my_addsub4/half_adder4.v
@@ -1,10 +1,6 @@
-module half_adder4(input signed [3:0] a, input signed [3:0] b, input c_in, 
-              output signed [3:0] sum, output c_out);
-wire [3:0] c;
-
-fulladder fa1(a[0],b[0], c_in, sum[0], c[1]) ;
-fulladder fa2(a[1],b[1], c[1], sum[1], c[2]) ;
-fulladder fa3(a[2],b[2], c[2], sum[2], c[3]) ;
-fulladder fa4(a[3],b[3], c[3], sum[3], c_out) ;
+module half_adder4(input signed [3:0] a, input b, 
+              output signed [3:0] s, output c);
+wire [3:0] o;
+half_adder ha1 (a[0],b,s[0],o[0]);
 
 endmodule
