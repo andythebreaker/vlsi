cf358dbf4b59feb3a020a43138764aad5424cf05
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-10-09 16:13:39 +0800

    4bit_add_sub_success_use_block_tset_ok

--- adder/my_addsub4/addSub4.v
+++ adder/my_addsub4/addSub4.v
@@ -6,8 +6,8 @@ wire [3:0] sum_withoutP;
 wire  Cout_withoutP;
 wire co;
 xor4 x1(b, {op,op,op,op}, contrast);
-adder4 a1(a, contrast, c_in, sum_withoutP, Cout_withoutP);
-half_adder4 ha1(sum_withoutP,op,sum,co);
-or(c_out,co,Cout_withoutP);
+adder4 a1(a, contrast, op, sum_withoutP,Cout_withoutP);
+half_adder4 ha1(sum_withoutP,c_in,sum,co);
+and(c_out,co,Cout_withoutP);
 
 endmodule
--- adder/my_addsub4/half_adder4.v
+++ adder/my_addsub4/half_adder4.v
@@ -1,8 +1,8 @@
 module half_adder4(input [3:0] a, input b, 
-              output [3:0] s, output c);
+              output [3:0] s,output c);
   wire [2:0] o;
 half_adder ha1 (a[0],b,s[0],o[0]);
-  half_adder ha2 (a[1],b,s[1],o[1]);
-  half_adder ha3 (a[2],b,s[2],o[2]);
-  half_adder ha4 (a[3],b,s[3],c);
+  half_adder ha2 (a[1],o[0],s[1],o[1]);
+  half_adder ha3 (a[2],o[1],s[2],o[2]);
+  half_adder ha4 (a[3],o[2],s[3],c);
 endmodule
