879742f0c4754e781749cf7e10afe5112278ee6d
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-10-14 14:13:05 +0800

    SHup20191014 Version = 25just_up_date_up.sh__cout_have_error_prob_exist

--- bnb_adder/my_addsub4/testbench.v
+++ bnb_adder/my_addsub4/testbench.v
@@ -7,12 +7,13 @@
 module testbench;
 reg signed [3:0] a;
 reg signed [3:0] b;
-wire signed [3:0] sum;
+wire signed [4:0] sum;
 reg op;
 reg c_in;
 wire c_out;
-wire signed [4:0] n;
-wire signed [4:0] num;
+//wire signed [5:0] n;
+//wire signed [4:0] num;
+//wire 
 
 addSub4 DUT (op, a, b,c_in, sum, c_out);
 
@@ -22,20 +23,21 @@ c_in=1'b0;
   a = 4'b0101;
   b = 4'b0000;
   op = 1'b0;
-  $monitor("%dns monitor: op=%d a=%d b=%d Cin=%b sum=%d Cout=%b num=%d", $stime, op, a, b,c_in, sum,c_out,num);
-
+  $monitor("%dns monitor: op=%d a=%d b=%d Cin=%b sum=%d Cout=%b num=%d", $stime, op, a, b,c_in, sum,c_out,sum);
+for()
 end
-assign n = c_out ? 5'd16 : 5'd0;
-assign num = n + sum;
+//assign num[5] = ( op == 1'b0 ) ? 1'b0 : ( sum[3] == 1'b1 ) ? 1'b1 : 1'b0;
+//assign num[4] = (op==1'b0)?c_out:;
+//assign num[3:0] = sum;
 
-always #50 begin
-  op=op+1;
-end
+//always #50 begin
+  //op=op+1;
+//end
 
-always #100 begin
-  b=b+1;
-end
+//always #100 begin
+  //b=b+1;
+//end
 
-initial #5000 $finish;
+//initial #5000 $finish;
 
 endmodule
