a23632244f9f66bf91d7808184beeb29b3caeb92
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-10-14 23:24:22 +0800

    SHup20191014 Version = 31-

---
+++ Behavior/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
@@ -0,0 +1,1 @@
+1571062270 /home/ncku_class/vlsi2019/vlsi2006/mydir/vlsi/Behavior/testbench.v
--- Behavior/adder_b.v
+++ Behavior/adder_b.v
@@ -1,22 +1,34 @@
 module adder_16bit_b(A , B ,Add_ctrl , O , C_out , SUM);
 
-input [15:00] A , B ;
+input [15:0] A , B ;
 input Add_ctrl;//1 for sub ,0 for add
 
 output [15:00] SUM ;
 output C_out ;
 output O ;
-  
-
-/** your code **/
-  wire [16:0] p16;
-  assign p16=(Add_ctrl==0)?(A+B):(A-B);
-  assign C_out=p16[16];
-  assign SUM=p16[15:0];
-  wire pn;
+reg aC_out;
+reg aO;
+reg [15:0] aSUM;
 assign e=A[15];
 assign r=B[15];
-assign pn=(Add_ctrl==1)?(((e==0)&&(r==1))||((e==1)&&(r==0)))?1:0:(((e==0)&&(r==0))||((e==1)&&(r==1)))?1:0;
-assign O=(pn==1)?(C_out==SUM[15])?0:1:0;
-  
+assign O=aO;
+assign C_out=aC_out;
+assign SUM=aSUM;
+
+/** your code **/
+
+always@(A or B or Add_ctrl)
+begin
+
+{aC_out,aSUM}=(Add_ctrl==0)?(A+B):(A-B);
+//{aC_out,aSUM}=(((Add_ctrl==1)?~B:B)+Add_ctrl)+A;
+end
+
+reg pn;
+always@(A or B or Add_ctrl)
+begin
+pn=(Add_ctrl==1)?(((e==0)&&(r==1))||((e==1)&&(r==0)))?1:0:(((e==0)&&(r==0))||((e==1)&&(r==1)))?1:0;
+aO=(pn==1)?(C_out==SUM[15])?0:1:0;
+end  
+
 endmodule
