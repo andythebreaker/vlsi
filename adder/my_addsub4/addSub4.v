21cbaee6a270ed3e4d3c111caf7a2e23cf67986d
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2019-10-14 14:23:31 +0800

    與ub方合併

--- adder/my_addsub4/addSub4.v
+++ adder/my_addsub4/addSub4.v
@@ -2,12 +2,13 @@ module addSub4(input op, input signed [3:0] a, input signed [3:0] b,input c_in,
                output signed [3:0] sum, output c_out);
 
 wire [3:0] contrast;
-wire [3:0] sum_withoutP;
-wire  Cout_withoutP;
+wire [3:0] comp2;
+//wire  Cout_withoutP;
 wire co;
 xor4 x1(b, {op,op,op,op}, contrast);
-adder4 a1(a, contrast, op, sum_withoutP,Cout_withoutP);
-half_adder4 ha1(sum_withoutP,c_in,sum,co);
-and(c_out,co,Cout_withoutP);
+half_adder4 ha1(contrast,op,comp2,co);
+adder4 a1(a, comp2, c_in, sum,c_out);
 
+//and(c_out,co,Cout_withoutP);
+//2comp no dot over flow
 endmodule
