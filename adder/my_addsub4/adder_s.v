6ffda657d7ac489c5a1036ef10edff19c8f27138
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2019-10-14 14:18:19 +0800

    Update adder_s.v

--- adder/my_addsub4/adder_s.v
+++ adder/my_addsub4/adder_s.v
@@ -6,9 +6,11 @@ input Add_ctrl;//1 for sub ,0 for add
 output [15:00] SUM ;
 output C_out ;
 output O ;
+  
+  wire [15:0] contrast;
 
-/** your code **/
-xor x1(b,{
+  /** your code **/
+  xor x1(b,{Add_ctrl,Add_ctrl,Add_ctrl,Add_ctrl,Add_ctrl,Add_ctrl,Add_ctrl,Add_ctrl,Add_ctrl,Add_ctrl,Add_ctrl,Add_ctrl,Add_ctrl,Add_ctrl,Add_ctrl,Add_ctrl},contrast);
 adder16 ()
 
 endmodule
