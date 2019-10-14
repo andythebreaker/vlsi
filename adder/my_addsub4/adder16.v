23606747f3790043a300406ad734dc2f4bbf6021
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2019-10-14 13:56:37 +0800

    補滿
    補滿

--- adder/my_addsub4/adder16.v
+++ adder/my_addsub4/adder16.v
@@ -3,5 +3,8 @@ module adder4(input signed [15:0] a, input signed [15:0] b, input c_in,
 wire [3:0] c;
 
 adder4 a1(a[3:0],b[3:0],c_in,sum[3:0],c[0]);
+  adder4 a2(a[7:4],b[7:4],c[0],sum[7:4],c[1]);
+  adder4 a3(a[11:8],b[11:8],c[1],sum[11:8],c[2]);
+  adder4 a4(a[15:12],b[15:12],c[2],sum[15:12],c_out);
 
 endmodule
