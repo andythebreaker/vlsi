2bd6f183864c7fd6ff790dafcae4651cdbbaf564
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-10-15 19:18:37 +0800

    SHup20191015 Version = 32   both B & S @ Add_Sub is ok!

--- Behavior/adder_b.v
+++ Behavior/adder_b.v
@@ -8,6 +8,9 @@ output C_out ;
 output O ;
 reg aC_out;
 reg aO;
+reg [16:0] idx;
+reg ws;
+reg [15:0] bp;
 reg [15:0] aSUM;
 assign e=A[15];
 assign r=B[15];
@@ -20,13 +23,21 @@ assign SUM=aSUM;
 always@(A or B or Add_ctrl)
 begin
 
-{aC_out,aSUM}=(Add_ctrl==0)?(A+B):(A-B);
-//{aC_out,aSUM}=(((Add_ctrl==1)?~B:B)+Add_ctrl)+A;
+aSUM=(Add_ctrl==0)?(A+B):(A-B);
+bp=(((Add_ctrl==1)?~B:B)+Add_ctrl);
+aC_out=1'b0;
+for(idx=0;idx<16;idx=idx+1)begin
+{aC_out,ws}={1'b0,bp[idx]}+{1'b0,A[idx]}+{1'b0,aC_out};
+//$display("this is test : idx= %d | aO= %b",idx,aO);
 end
+
+end
+
+
 
 reg pn;
 always@(A or B or Add_ctrl)
-begin
+#1 begin
 pn=(Add_ctrl==1)?(((e==0)&&(r==1))||((e==1)&&(r==0)))?1:0:(((e==0)&&(r==0))||((e==1)&&(r==1)))?1:0;
 aO=(pn==1)?(C_out==SUM[15])?0:1:0;
 end  
