5e88f9aa2d2b6ebdb2d742313fc609eceefad430
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-10-26 00:32:03 +0800

    SHup20191026 Version = 44

---
+++ adder/fulladder/Ntest_bench.v
@@ -0,0 +1,63 @@
+`include "fulladder.v"
+`timescale 1 ns / 1 ps
+module test_bench;
+reg a;
+reg b;
+reg c;
+wire s;
+wire co;
+fulladder FA (a,b,c,s,co);
+initial
+begin
+$display("=========This is a test bench of FULL ADDER=========");
+$monitor($time," || a= %b | b= %b | c= %b | s= %b | os= %b",a,b,c,s,co);
+a=1'b0;
+b=1'b0;
+c=1'b0;
+#5 a=1'b0;
+b=1'b0;
+c=1'b1;
+  #5 a=1'b0;
+b=1'b1;
+c=1'b0;
+  #5 a=1'b1;
+b=1'b0;
+c=1'b0;
+  #5 a=1'b0;
+b=1'b1;
+c=1'b1;
+  #5 a=1'b1;
+b=1'b1;
+c=1'b0;
+  #5 a=1'b1;
+b=1'b0;
+c=1'b1;
+  #5 a=1'b1;
+b=1'b1;
+c=1'b1;
+#5 $finish;
+end
+//====================chart========================
+//for all abc -> output S/C
+//000->0/0
+//001->1/0
+//011->0/1
+//111->1/1
+//a|b|c/S-c
+//000/0-0
+//001/1-0
+//010/1-0
+//100/1-0
+//011/0-1
+//110/0-1
+//101/0-1
+//111/1-1
+//============all we need to test==================
+//always #50 begin
+//if
+//initial
+//begin
+//$dumpfile("fulladder.vcd");
+//$dumpvars;
+//end
+endmodule
