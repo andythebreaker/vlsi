6a9c30a6fbe6a0c0eebd5f5124ad6c05af794d38
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-11-19 21:23:43 +0800

    SHup20191119 Version = 5no msg!

--- npl_cpu/INCA_libs/irun.lnx86.15.20.nc/files.ts
+++ npl_cpu/INCA_libs/irun.lnx86.15.20.nc/files.ts
@@ -1,1 +1,2 @@
-1574168805 /home/ncku_class/vlsi2019/vlsi2006/mydir/vlsi/npl_cpu/nplcpu.v
+1574169641 /home/ncku_class/vlsi2019/vlsi2006/mydir/vlsi/npl_cpu/nplcpu.v
+1574169641 /home/ncku_class/vlsi2019/vlsi2006/mydir/vlsi/npl_cpu/instruction_set_model.v
